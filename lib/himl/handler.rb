d5e0d2e04f44aab7215582f36986e7d66341c982
Akira Matsuda <ronnie@dio.jp>
2019-03-14 12:37:42 +0900

    Action View 6 support

--- lib/himl/handler.rb
+++ lib/himl/handler.rb
@@ -2,20 +2,23 @@
 
 module Himl
   class Handler
+    ERB_HANDLER = ActionView::Template.handler_for_extension('erb')
+
     def self.call(template, source = nil)
-      new.call(source || template.source)
+      new.call(template, source || template.source)
     end
 
-    def call(source)
+    def call(template, source)
       parser = Himl::Parser.new
       parser.call(source)
       erb = parser.to_erb
-      erb_handler = Template.handler_for_extension('erb')
 
-      erb_handler.erb_implementation.new(
+      escape = ERB_HANDLER.respond_to?(:escape_ignore_list) ? ERB_HANDLER.escape_ignore_list.include?(template.type) : ERB_HANDLER.escape_whitelist.include?(template.type)
+
+      ERB_HANDLER.erb_implementation.new(
         erb,
-        escape: (erb_handler.escape_ignore_list.include? template.type),
-        trim: (erb_handler.erb_trim_mode == "-")
+        escape: escape,
+        trim: (ERB_HANDLER.erb_trim_mode == "-")
       ).src
     end
   end
