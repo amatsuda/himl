3406873563174463c6de4703b43690313fba4d68
Akira Matsuda <ronnie@dio.jp>
2019-03-14 18:50:26 +0900

    Better not depend on rails

--- himl.gemspec
+++ himl.gemspec
@@ -37,7 +37,6 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_dependency 'nokogiri'
-  spec.add_dependency 'rails'
 
   spec.add_development_dependency "bundler", "~> 2.0"
   spec.add_development_dependency 'rake'
--- lib/himl/railtie.rb
+++ lib/himl/railtie.rb
@@ -1,15 +1,19 @@
 # frozen_string_literal: true
 
-require 'rails'
+begin
+  require 'rails'
 
-module Himl
-  class Railtie < ::Rails::Railtie
-    initializer 'himl' do
-      ActiveSupport.on_load :action_view do
-        require_relative 'handler'
+  module Himl
+    class Railtie < ::Rails::Railtie
+      initializer 'himl' do
+        ActiveSupport.on_load :action_view do
+          require_relative 'handler'
 
-        ActionView::Template.register_template_handler :himl, Himl::Handler
+          ActionView::Template.register_template_handler :himl, Himl::Handler
+        end
       end
     end
   end
+rescue LoadError
+  # do nothing
 end
